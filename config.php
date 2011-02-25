01e335386853d3603866fa2be1e730617e3e0ebc
Ben Walker <ben@ihatemornings.com>
2011-02-25 11:14:01 +0000

    Added A Silent Film and Pick Your Weapon

--- config.php
+++ config.php
@@ -1,22 +1,62 @@
 <?php
 	
-	$myzbase["littlefishmusic"] = Array(
-		"name" => "Little Fish",
-		"description" => "A two-piece garage-rock band from Oxford",
-		"soundcloud_set_url" => "http://soundcloud.com/littlefishmusic/sets/baffled-and-beat",
-		"songkick_id" => "2505871",
-		"photo" => "lf-bg.jpg",
-		"website_url" => "http://littlefishmusic.com",
-		"contacts" => Array(
-			Array(
-				"type" => "Management and enquires",
-				"email" => "littlefish@trinifold.co.uk"
-			),
-			Array(
-				"type" => "Booking agent",
-				"email" => "phyllis@itb.co.uk"
-			)
+$myzbase["littlefishmusic"] = Array(
+	"name" => "Little Fish",
+	"description" => "A two-piece garage-rock band from Oxford",
+	"soundcloud_set_url" => "http://soundcloud.com/littlefishmusic/sets/baffled-and-beat",
+	"songkick_id" => "2505871",
+	"photo" => "lf-bg.jpg",
+	"website_url" => "http://littlefishmusic.com",
+	"contacts" => Array(
+		Array(
+			"type" => "Management and enquires",
+			"email" => "littlefish@trinifold.co.uk"
+		),
+		Array(
+			"type" => "Booking agent",
+			"email" => "phyllis@itb.co.uk"
 		)
-	);
-	
+	)
+);
+
+$myzbase["asilentfilm"] = Array(
+	"name" => "A Silent Film",
+	"description" => "A rock band from Oxford, England",
+	"soundcloud_set_url" => "http://soundcloud.com/a-silent-film/sets/the-city-that-sleeps/",
+	"songkick_id" => "87735",
+	"photo" => "asilentfilm.jpg",
+	"website_url" => "http://www.asilentfilm.com",
+	"contacts" => Array(
+		Array(
+			"type" => "Management",
+			"email" => "john@marsupialmanagement.com"
+		),
+		Array(
+			"type" => "Agent UK",
+			"email" => "joanna@itb.co.uk"
+		),
+		Array(
+			"type" => "Agent US",
+			"email" => "dklein@caa.com"
+		)
+	)
+);
+
+$myzbase["pickyourweapon"] = Array(
+	"name" => "Pick Your Weapon",
+	"description" => "A punk rock band. We sing and shout.",
+	"soundcloud_set_url" => "http://soundcloud.com/roundonefight/sets/pick-your-weapon",
+	"songkick_id" => "3483916",
+	"photo" => "pickyourweapon.jpg",
+	"website_url" => "http://pickyourweapon.co.uk",
+	"contacts" => Array(
+		Array(
+			"type" => "Band",
+			"email" => "pickyourweapon@gmail.com"
+		)
+	)
+);
+
+
+
 ?>

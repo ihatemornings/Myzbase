1c1466d97619236928942005961da24a01691099
Ben Walker <ben@ihatemornings.com>
2011-03-12 18:23:40 +0000

    Updated Little Fish description

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 	
 $myzbase["littlefishmusic"] = Array(
 	"name" => "Little Fish",
-	"description" => "A two-piece garage-rock band from Oxford",
+	"description" => "A three-piece garage-rock band from Oxford",
 	"soundcloud_set_url" => "http://soundcloud.com/littlefishmusic/sets/baffled-and-beat",
 	"songkick_id" => "2505871",
 	"photo" => "littlefishmusic.jpg",
